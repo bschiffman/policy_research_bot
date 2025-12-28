dd50a14378b100e3a67d71ab4dbb36ae1ffda4c4
bschiffman <bschiffman@uchicago.edu>
2025-12-28 15:11:10 -0500

    fetch semantic scholar creation

---
+++ src/fetch_semantic_scholar.py
@@ -0,0 +1,30 @@
+import requests
+from datetime import datetime, timedelta
+
+SEMANTIC_API = "https://api.semanticscholar.org/graph/v1/paper/search"
+
+def fetch_semantic_papers(query, days_back=7, limit=5):
+    since = (datetime.utcnow() - timedelta(days=days_back)).strftime("%Y-%m-%d")
+
+    params = {
+        "query": query,
+        "limit": limit,
+        "fields": "title,authors,abstract,year,venue,url",
+    }
+
+    r = requests.get(SEMANTIC_API, params=params)
+    r.raise_for_status()
+
+    papers = []
+    for p in r.json().get("data", []):
+        papers.append({
+            "id": p["paperId"],
+            "title": p["title"],
+            "authors": ", ".join(a["name"] for a in p.get("authors", [])),
+            "abstract": p.get("abstract", ""),
+            "source": "Semantic Scholar",
+            "url": p.get("url"),
+            "year": p.get("year"),
+        })
+
+    return papers
